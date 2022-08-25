9aff4e6f2700c2a6595b6a3d5da83c8abcd94fa6
carloskauan <carloskauan190@gmail.com>
2022-08-25 14:42:06 -0300

    add update noderap

--- nodegeek/noderap/src/index.js
+++ nodegeek/noderap/src/index.js
@@ -26,6 +26,24 @@ app.get('/raps', (req, res) => {
     return res.status(200).json(allraps)
 });
 
+app.get('/raps/:rap_id', (req, res) => {
+    const { rap_id } = req.params
+    // filtragem do id
+    const rap = raps.find((rap) => rap.id === rap_id);
+
+    if(!rap) res.status(404).json('not found')
+
+    return res.status(200).json(rap);
+})
+
+app.delete('/raps/:rap_id', (req, res) => {
+    const { rap_id } = req.params;
+    const filteredrap = raps.filter((rap) => rap.id !== rap_id )
+    raps = filteredrap
+    
+    return res.status(204).json('deleted')
+}); 
+
 // listen
 app.listen(3333, (req, res)=> {
     console.log('running')
